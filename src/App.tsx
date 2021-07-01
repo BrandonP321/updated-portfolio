a05ba8e3c286630db87a73210433fd65759323f6
Brandon Phillips <brandon.phillips976@gmail.com>
2021-06-30 21:58:11 -0700

    Added ResponsiveInternal class with methods to create responsive classnames in html element
    Created class ResponsiveInteral class with method that adds classes to the <html> element to reflect the current width of the screen for styling purposed

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import { Responsive } from './utils/Responsive';
 function App() {
   useEffect(() => {
     // update html with class reflecting current size of the screen
-    Responsive.Instantiate().addResponsiveSizeListeners();
+    Responsive.addResponsiveSizeListeners();
   }, [])
 
   return (
--- src/utils/Responsive.ts
+++ src/utils/Responsive.ts
@@ -1,36 +1,71 @@
-export class Responsive {
-    static readonly responsiveMediaQueries: { [key: string]: string } = {
-        extralarge: "(min-width: 2501px)",
-        large: "(max-width: 1700px)",
-        medium: "(max-width: 1300px)",
-        mobile: "(max-width: 850px)",
-        tiny: "(max-width: 450px)",
-    } as const;
+type ResponsiveSize = "massive" | "extralarge" | "large" | "medium" | "mobile" | "tiny";
+
+class ResponsiveMediaQuery {
+    public name: ResponsiveSize;
+    public query: string;
+
+    constructor(name: ResponsiveSize, query: string) {
+        this.name = name;
+        this.query = query;
+    }
+
+    test() {
+        const mm = window.matchMedia(this.query);
+        return mm.matches;
+    }
+
+    createMatchMedia() {
+        return window.matchMedia(this.query);
+    }
+}
+
+class ResponsiveInternal {
+    private _massive = new ResponsiveMediaQuery("massive", "screen and (min-width: 2500px)");
+    private _extraLarge = new ResponsiveMediaQuery("extralarge", "screen and (max-width: 1600px)");
+    private _large = new ResponsiveMediaQuery("large", "screen and (max-width: 1300px)");
+    private _medium = new ResponsiveMediaQuery("medium", "screen and (max-width: 1000px)");
+    private _mobile = new ResponsiveMediaQuery("mobile", "screen and (max-width: 800px)");
+    private _tiny = new ResponsiveMediaQuery("tiny", "screen and (max-width: 450px)");
+
+    get massive() { return this._massive.test() };
+    get extraLarge() { return this._extraLarge.test() };
+    get large() { return this._large.test() };
+    get medium() { return this._medium.test() };
+    get mobile() { return this._mobile.test() };
+    get tiny() { return this._tiny.test() };
 
     private responsiveSizes: { [key: string]: boolean } = {};
+    private mediaQueries: ResponsiveMediaQuery[] = []
 
-    public static Instantiate() {
-        return new Responsive();
+    constructor() {
+        this.mediaQueries = [
+            this._massive,
+            this._extraLarge,
+            this._large,
+            this._medium,
+            this._mobile,
+            this._tiny
+        ]
+    }
+
+    public static Instance() {
+        return new ResponsiveInternal();
     }
 
     addResponsiveSizeListeners() {
-        Object.keys(Responsive.responsiveMediaQueries).forEach(size => {
-            const mediaQuery = window.matchMedia(Responsive.responsiveMediaQueries[size])
-
-            // add listener for media query change
-            mediaQuery.addEventListener("change", (e) => this.handleQueryChange(e, size))
-
-            // make initial check for media query
-            this.handleInitalQueryCheck(mediaQuery, size);
+        this.mediaQueries.forEach(query => {
+            const mediaQuery = query.createMatchMedia();
+            mediaQuery.addEventListener("change", (e) => this.handleQueryChange(e, query.name))
+            this.handleInitalQueryCheck(mediaQuery, query.name)
         })
     }
 
-    private handleInitalQueryCheck(query: MediaQueryList, size: string) {
+    private handleInitalQueryCheck(query: MediaQueryList, size: ResponsiveSize) {
         this.responsiveSizes[`r-${size}`] = query.matches;
         this.updateHTMLClass();
     }
 
-    private handleQueryChange(e: MediaQueryListEvent, size: string) {
+    private handleQueryChange(e: MediaQueryListEvent, size: ResponsiveSize) {
         this.responsiveSizes[`r-${size}`] = e.matches;
         this.updateHTMLClass();
     }
@@ -39,6 +74,21 @@ export class Responsive {
      * update <html> class with responsive size classes to reflect current size of the screen
      */
     private updateHTMLClass() {
-        document.documentElement.className = Object.keys(this.responsiveSizes).filter(size => this.responsiveSizes[size]).join(" ");
+        // document.documentElement.className = Object.keys(this.responsiveSizes).filter(size => this.responsiveSizes[size]).join(" ");
+        Object.keys(this.responsiveSizes).forEach(size => {
+            const doesQueryMatch = this.responsiveSizes[size];
+            const htmlEle = document.documentElement;
+            // check if <html> has class for this specific size
+            const htmlEleHasSizeClass = htmlEle.classList.contains(size)
+
+            if (doesQueryMatch && !htmlEleHasSizeClass) {
+                console.log("adding class")
+                htmlEle.classList.add(size)
+            } else if (!doesQueryMatch && htmlEleHasSizeClass) {
+                htmlEle.classList.remove(size);
+            }
+        })
     }
 }
+
+export const Responsive = ResponsiveInternal.Instance();
