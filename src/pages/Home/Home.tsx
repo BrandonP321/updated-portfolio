19a408a359b9b2dffdb25107f1803f4f60ddda4f
Brandon Phillips <brandon.phillips976@gmail.com>
2021-07-03 11:14:45 -0700

    Added new hero component
    Added a second hero component with a parallaxing background

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,16 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useRef } from 'react'
+import { Responsive } from '../../utils/Responsive'
+// import Hero from './sections/Hero/Hero';
+import Hero from './sections/Hero1/Hero';
+import styles from "./Home.module.scss";
 
-interface Props {
-    
-}
+export default function Home(): ReactElement {
+    const { current: responsive } = useRef(Responsive);
 
-export default function Home({}: Props): ReactElement {
     return (
-        <div>
-            <h1>Home</h1>
+        <div className={styles.home}>
+            <Hero/>
+            <div className={styles.testBody}/>
         </div>
     )
 }
---
+++ src/pages/Home/sections/Hero1/Hero.tsx
@@ -0,0 +1,40 @@
+import React, { ReactElement } from 'react'
+import { useEffect } from 'react';
+import { useState } from 'react';
+import styles from "./Hero.module.scss";
+
+interface Props {
+    
+}
+
+export default function Hero({}: Props): ReactElement {
+    const [parallaxAmount, setParallaxAmount] = useState(0);
+
+    useEffect(() => {
+        const parallaxSpeed = .25;
+
+        // make initial parallax call on page load
+        parallax(parallaxSpeed);
+
+        window.addEventListener("scroll", () => parallax(parallaxSpeed))
+
+        return () => window.removeEventListener("scroll", () => parallax);
+    }, [])
+
+    const parallax = (scrollRate: number) => {
+        setParallaxAmount(window.pageYOffset * scrollRate);
+    }
+
+    const heroStyle: React.CSSProperties = {
+        backgroundPosition: `center, 50% ${parallaxAmount}px`
+    }
+
+    return (
+        <div className={styles.hero} style={heroStyle}>
+            <div className={styles.heroContent}>
+                <h1 className={styles.title}>BRANDON PHILLIPS</h1>
+                <p className={styles.subtitle}>FULL STACK WEB DEVELOPER</p>
+            </div>
+        </div>
+    )
+}
